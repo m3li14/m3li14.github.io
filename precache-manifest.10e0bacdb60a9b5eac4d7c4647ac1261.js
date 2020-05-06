6af9b291edb4c266413578def2a4a22dfd97830c
Ayman Ali <aliayman@Aymans-MacBook-Pro13.local>
2020-05-05 18:55:35 -0700

    Updates

---
+++ precache-manifest.10e0bacdb60a9b5eac4d7c4647ac1261.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3b7a9a5f6de5352d473d7836707e76d3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f6f9565503648a08bc64",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "d665dc60cd1a4aa96964",
+    "url": "/static/js/2.0b1bf08b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.0b1bf08b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6f9565503648a08bc64",
+    "url": "/static/js/main.f81b9322.chunk.js"
+  },
+  {
+    "revision": "b40d0ef6ae217a7c1c49",
+    "url": "/static/js/runtime-main.def81cf6.js"
+  }
+]);
